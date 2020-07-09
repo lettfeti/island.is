ec4a00392f8c1bed5164c24df8efcfb9e65399f1
Andri Andresson <andri@sendiradid.is>
2020-07-09 13:23:09 +0000

    feat: updated types

--- libs/service-portal/types/src/lib/service-portal.d.ts
+++ libs/service-portal/types/src/lib/service-portal.d.ts
@@ -4,13 +4,27 @@ export interface ServicePortalNavigation {
   routes: ServicePortalNavItem[]
 }
 export interface ServicePortalNavItem {
+  group?: 'action' | 'information'
   label: string
   path: string
+  navItems?: Array<ServicePortalNavItem>
 }
 
-interface ServicePortalModule {
-  getRoutes?: (navItems: ServicePortalNavItem[]) => void
+export interface ServicePortalModuleRoute {
+  group?: 'action' | 'information'
+  label: string
+  path: string
   scope: string[]
+  Component: React.FC<any>
+  navItems?: Array<ServicePortalModuleRoute>
 }
 
 export type ServicePortalModuleProps = FC<ServicePortalModule>
+
+export interface ServicePortalModule {
+  (scope: Array<string>): {
+    component: React.FC<any>
+    dashboard: React.Component[]
+    navItems: ServicePortalNavItem[]
+  }
+}
