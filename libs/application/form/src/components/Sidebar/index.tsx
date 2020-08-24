ecf9a4f30ee5515d438770bc199263ca70ee9c1c
Noam <noam@aranja.com>
2020-08-24 14:18:17 +0000

    Start work on mobile sidebar states

--- libs/application/form/src/components/Sidebar/index.tsx
+++ libs/application/form/src/components/Sidebar/index.tsx
@@ -1,18 +1,21 @@
-import React, { FC } from 'react'
+import React, { FC, useState } from 'react'
 import { Box } from '@island.is/island-ui/core'
 import * as styles from './Sidebar.treat'
 
-const Sidebar: FC<{ children: React.ReactNode }> = ({ children }) => (
-  <Box
-    background="purple100"
-    borderRadius="large"
-    paddingX={4}
-    paddingTop={3}
-    paddingBottom={2}
-    className={styles.root}
-  >
-    {children}
-  </Box>
-)
+const Sidebar: FC<{ children: React.ReactNode }> = ({ children }) => {
+  const [isExpanded, setIsExpanded] = useState(false)
+  return (
+    <Box
+      background="purple100"
+      borderRadius="large"
+      paddingX={4}
+      paddingTop={3}
+      paddingBottom={2}
+      className={styles.root}
+    >
+      {children}
+    </Box>
+  )
+}
 
 export default Sidebar
